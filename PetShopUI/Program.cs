259c02991f6e1b8aa3338847d2a7f416730cc875
Mathias Mols Thiim <math75n9@easv.dk>
2021-08-25 11:59:58 +0200

    Update Program.cs

--- PetShopUI/Program.cs
+++ PetShopUI/Program.cs
@@ -6,7 +6,22 @@ namespace PetMenu
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            FakeDB.InitData();
+            
+            var serviceCollection = new ServiceCollection();
+            serviceCollection.AddScoped<ICarRepository, CarRepository>();
+            serviceCollection.AddScoped<ICarMakeRepository, CarMakeRepository>();
+            serviceCollection.AddScoped<ICarService, CarService>();
+
+            var serviceProvider = serviceCollection.BuildServiceProvider();
+            var carService = serviceProvider.GetRequiredService<ICarService>();
+            new Printer(carService);
+
+            Console.ReadLine();
+            /*////then build provider 
+            var serviceProvider = serviceCollection.BuildServiceProvider();
+            var printer = serviceProvider.GetRequiredService<IPrinter>();
+            printer.StartUI();*/
         }
     }
 }
