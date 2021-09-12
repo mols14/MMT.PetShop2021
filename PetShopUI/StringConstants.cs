4a981e64e1486c902cb5d35233cbcba3aad6bd82
Mathias M. Thiim <mathias_thiim@hotmail.com>
2021-09-13 00:02:00 +0200

    Update StringConstants.cs

--- PetShopUI/StringConstants.cs
+++ PetShopUI/StringConstants.cs
@@ -2,6 +2,21 @@ namespace PetMenu
 {
     public class StringConstants
     {
-        
+        public const string Greet = "Welcome to the petshop. Select a number to continue...";
+        public const string ShowAllPets = "1. Show all pets";
+        public const string CreatePet = "2. Create a new pet";
+        public const string DeletePet = "3. Delete a pet";
+        public const string UpdatePet = "4. Update a pet";
+        public const string SearchByType = "5. Search by pet type";
+        public const string ShowMostExpensivePets = "6. Show the most expensive pets";
+        public const string ShowCheapestPets = "7. Show the cheapest pets";
+        public const string ExitApplication = "0. Exit Application";
+        public const string Id = "Give the pet an Id:";
+        public const string Name = "Give the pet a name";
+        public const string Price = "Type the price of the pet";
+        public const string Type = "Pick the type of pet";
+        public const string Color = "Type the color of the pet";
+        public const string BirthDate = "Type the birthdate of the pet in the following format: dd/mm/yyyy";
+        public const string SoldDate = "Type the date the pet was sold in the following format: dd/mm/yyyy";
     }
 }
